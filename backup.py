74e2ecc046726e5151281897a2f9bccdec42a3b8
Hai Ninh Hoang <haininhhoang94@gmail.com>
2020-08-04 14:51:43 +0700

    add chow_test add hv plot for nyse_close correct 10 years nyse add code to read local data first before get from api

--- backup.py
+++ backup.py
@@ -1,15 +1,5 @@
-# get data covid_19
-# while True:
-#     try:
-#         with open(p + "/Data/all_location.txt") as outfile:
-#             all_location_json = json.load(outfile)
-#         break
-#     except FileNotFoundError:
-#         covid = cpy.COVID19(url="https://covid-tracker-us.herokuapp.com")
-#         # US_covid = covid.getLocationByCountryCode("US", timelines=True)
-#         # vir = dict(US_covid["latest"])
-#         # all_location_json = covid.getAll(timelines=True)
-#         all_location_json = covid.getLocationByCountryCode("US")(timelines=True)
-#         with open(p + "/Data/all_location.txt", "w") as outfile:
-#             json.dump(all_location_json, outfile)
+y1 = nyse_hist[nyse_hist.index < 2020]["Volume"]
+x1 = nyse_hist[nyse_hist.index < 2020]["Close"]
+y2 = nyse_hist[nyse_hist.index >= 2020]["Volume"]
+x2 = nyse_hist[nyse_hist.index >= 2020]["Close"]
 
